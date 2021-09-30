124ada37c17a6e2cf2b5de11d38cabd51d9c7eb8
Angus B <38348730+LetsCode-Angus@users.noreply.github.com>
2021-09-30 18:35:39 +1000

    Architectural Updates
    Moved to a scene based architecture

---
+++ main_scene.py
@@ -0,0 +1,102 @@
+import sys, pygame
+from pygame.key import start_text_input
+from pygame.locals import *
+from pygame.time import *
+import colors
+from gameplay_functions import *
+from helper_functions import *
+from config import DEFAULT_CONFIG_PATH, config, default_config
+from scenes import *
+
+class main_scene(scene):
+
+    def awake(self):
+        # Init nonsense
+        self.DEFAULT_SETTINGS = config('DEFAULT_SETTINGS')
+        self.settings = default_config(self.DEFAULT_SETTINGS, 'user_settings.cfg')
+        self.FPS_MAX = self.settings.get("FPS_MAX")
+        self.FONT_MAIN = self.settings.get("FONT_MAIN")
+
+        pygame.init()
+        self.fnt_comic_sans_30 = pygame.font.SysFont(self.FONT_MAIN, 30)
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption('PenIsClicker')
+        self.img_balls = pygame.image.load('images/balls.png')
+        pygame.display.set_icon(self.img_balls)
+
+        # Set up screen
+        self.screen_size = width, height = 800, 450
+        self.screen = pygame.display.set_mode(self.screen_size)
+
+        # Initialise images and their rects
+        self.img_pen = pygame.image.load("images/pen.png")
+        self.rect_pen = self.img_pen.get_rect() # Gets dimensions of rect object (left, top, height, width)
+        self.img_pen_clicked = pygame.image.load("images/pen_clicked.png")
+        self.rect_pen_clicked = self.img_pen_clicked.get_rect()
+
+        self.img_bg = pygame.image.load("images/Background.png")
+        self.rect_bg = self.img_bg.get_rect()
+
+        self.img_title = pygame.image.load("images/title_screen.png")
+        self.rect_title = self.img_title.get_rect()
+
+        self.clicks = get_clicks() # Click counter
+
+    def update(self):
+        
+        clicked = False # Pen is clicked or not
+        fps = 0
+        cps = 0
+        self.time = 0
+
+        fps = round(self.clock.get_fps(), 2)
+
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT: 
+                self.end_scene()
+                self.settings.save()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    self.clicks += 1 * multiplier(self.clicks)
+                    clicked = click_pen(clicked)
+                    self.time = update_clicks_per_second()
+                    
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    if pygame.Rect.collidepoint(self.rect_pen, event.pos):
+                        self.clicks += 1 * multiplier(self.clicks)
+                        clicked = click_pen(clicked)
+                        self.time = update_clicks_per_second()
+
+        self.cps = round(get_clicks_per_second(), 1)
+
+        txt_click_count = self.fnt_comic_sans_30.render(str(self.clicks), False, colors.BLUE)
+        txt_multiplier = self.fnt_comic_sans_30.render("click multiplier: " + str(multiplier(self.clicks)), False, colors.BLUE)
+        txt_fps = self.fnt_comic_sans_30.render("fps " + str(fps), False, colors.RED)
+        txt_cps = self.fnt_comic_sans_30.render("Clicks per second:  " + str(cps), False, colors.BLUE)
+
+        self.clock.tick(self.FPS_MAX)
+        
+        self.screen.fill(colors.ERROR_PINK) # If this color shows through, something is wrong
+        self.screen.blit(self.img_bg, self.rect_bg)
+
+        tp_cntr = get_centred_x_coords(txt_click_count.get_rect(), self.screen)
+        self.screen.blit(txt_click_count, tp_cntr)
+        txt_multiplier_pos = get_centred_x_coords(txt_multiplier.get_rect(), self.screen)
+        txt_multiplier_pos.y += 30
+        self.screen.blit(txt_multiplier, txt_multiplier_pos)
+
+        txt_cps_pos = get_centred_x_coords(txt_cps.get_rect(), self.screen)
+        txt_cps_pos.y += 60
+        self.screen.blit(txt_cps, txt_cps_pos)
+
+        if clicked:
+            self.screen.blit(self.img_pen_clicked, get_centred_coords(self.rect_pen, self.screen))
+        else:
+            self.screen.blit(self.img_pen, get_centred_coords(self.rect_pen, self.screen))
+        self.screen.blit(txt_fps, (0,0))
+        pygame.display.flip() # Update full display to the screen
+
+        save_clicks(self.clicks)
--- scenes.py
+++ scenes.py
@@ -7,6 +7,7 @@ class is_serialisable:
     def load() -> object:
         raise NotImplemented("The load function has not been implemented on this object")
 
+# TODO: Use typings to create statically typed args parser
 class delegate():
 
     def __init__(self):
@@ -16,18 +17,70 @@ class delegate():
         for func in self.funcs:
             func()
 
+    def subscribe(self, callback):
+        self.funcs.append(callback)
+    
+    def unsubscribe(self, callback):
+        self.funcs.remove(callback)
+
+    def __iadd__(self, callback):
+        self.subscribe(callback)
+        return self
+    
+    def __isub__(self, callback):
+        self.unsubscribe(callback)
+        return self
+
+    def __str__(self) -> str:
+        return "delegate object with " + str(len(self.funcs)) + " callbacks"
+    
+    def __repr__(self) -> str:
+        return self.__str__()
+
+
 class scene(is_serialisable):
 
     def __new__(cls, *args, **kwargs):
         scene_obj= super(scene, cls).__new__(cls)
-        scene_obj.__setattr__('on_awake', "hello world")
+        scene_obj.__setattr__('on_awake', delegate())
+        scene_obj.__setattr__('on_update', delegate())
+        scene_obj.__setattr__('on_end', delegate())
+        print("created scene " +  str(scene_obj.on_update))
         return scene_obj
 
-    def __init__(self, name):
-        print(self.test)
+    def __init__(self):
+        self.on_awake += self.awake
+        self.on_update += self.update
+        print(self.on_awake)
 
-    def awake():
+    def awake(self):
         pass
 
-    def update():
+    def update(self):
         pass
+
+    def end_scene(self):
+        self.on_end()
+        self._running = False
+
+    @property
+    def is_running(self):
+        return self._running
+
+    def _awake(self):
+        self._running = True
+        self.on_awake()
+
+    def _update(self):
+        self.on_update()
+
+class scene_manager:
+
+    def __init__(self):
+        pass
+
+    def load_scene(self, scene):
+        scene._awake()
+
+        while scene.is_running:
+            scene._update()
